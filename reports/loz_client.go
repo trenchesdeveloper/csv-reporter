28ab06512bb5d06a2a641f1840c33d8f6808d513
trenchesdeveloper <opeyemisamuel222@gmail.com>
2025-05-24 09:40:25 +0100

    remove the volume folder

--- reports/loz_client.go
+++ reports/loz_client.go
@@ -1,1 +1,35 @@
 package reports
+
+import "net/http"
+
+const baseUrl = "https://botw-compendium.herokuapp.com/api/v3/compendium"
+
+type HttpClient interface {
+	Do(*http.Request) (*http.Response, error)
+}
+
+type Client struct {
+	httpClient HttpClient
+	baseURL    string
+}
+
+func NewClient(baseUrl string, httpClient HttpClient) *Client {
+	if baseUrl == "" {
+		baseUrl = "https://botw-compendium.herokuapp.com/api/v3/compendium"
+	}
+	return &Client{
+		httpClient: httpClient,
+		baseURL:    baseUrl,
+	}
+}
+
+type Monster struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+	Id 		int   `json:"id"`
+	Category    string `json:"category"`
+	CommonLocations []string `json:"common_locations"`
+	Drops	   []string `json:"drops"`
+	Dlc bool `json:"dlc"`
+}
